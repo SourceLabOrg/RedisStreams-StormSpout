08b9b53335b28bdfdb7e92caa038cdbfb2d64850
Crim <stephen.powis@gmail.com>
2020-07-13 17:36:02 +0900

    Add test coverage

---
+++ src/test/java/org/sourcelab/storm/spout/redis/client/ConsumerTest.java
@@ -0,0 +1,189 @@
+package org.sourcelab.storm.spout.redis.client;
+
+import org.junit.Rule;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
+import org.junit.rules.Timeout;
+import org.mockito.Mockito;
+import org.sourcelab.storm.spout.redis.Configuration;
+import org.sourcelab.storm.spout.redis.Message;
+import org.sourcelab.storm.spout.redis.failhandler.NoRetryHandler;
+import org.sourcelab.storm.spout.redis.funnel.ConsumerFunnel;
+import org.sourcelab.storm.spout.redis.funnel.MemoryFunnel;
+import org.sourcelab.storm.spout.redis.util.TestTupleConverter;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static java.time.Duration.ofMillis;
+import static java.time.Duration.ofSeconds;
+import static org.junit.Assert.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertTimeout;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.ArgumentMatchers.longThat;
+import static org.mockito.Mockito.atLeast;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+import static org.mockito.Mockito.when;
+
+/**
+ * Tests over LettuceClient using a mock RedisClient instance.
+ */
+class ConsumerTest {
+    private static final String HOSTNAME = "my.example.host.com";
+    private static final int PORT = 1234;
+    private static final String STREAM_KEY = "StreamKeyValue";
+
+    // Create config
+    private final Configuration config = Configuration.newBuilder()
+        .withHost(HOSTNAME)
+        .withPort(PORT)
+        .withStreamKey(STREAM_KEY)
+        .withGroupName("GroupName")
+        .withConsumerId("ConsumerId")
+        .withFailureHandlerClass(NoRetryHandler.class)
+        .withTupleConverterClass(TestTupleConverter.class)
+        // Small delay.
+        .withConsumerDelayMillis(10L)
+        .build();
+
+    private MemoryFunnel funnel;
+
+    // Mocks
+    private Client mockClient;
+
+    // Instance under test
+    private Consumer consumer;
+
+
+    @BeforeEach
+    void setup() {
+        funnel = new MemoryFunnel(config, new HashMap<>());
+        mockClient = mock(Client.class);
+        consumer = new Consumer(config, mockClient, funnel);
+
+
+    }
+
+    @AfterEach
+    void cleanup() {
+        // Ensure all interactions accounted for.
+        verifyNoMoreInteractions(mockClient);
+    }
+
+    @Test
+    void smokeTest() throws InterruptedException {
+        // Setup return values
+        final List<Message> firstMessageBatch = createMessageBatch(10, 0);
+        final List<Message> secondMessageBatch = Collections.emptyList();
+        final List<Message> thirdMessageBatch = createMessageBatch(20, 10);
+
+        // All further calls just get an empty list
+        final List<Message> finalBatch = Collections.emptyList();
+
+        // Setup Mocks
+        when(mockClient.nextMessages()).thenReturn(
+            firstMessageBatch, secondMessageBatch, thirdMessageBatch, finalBatch
+        );
+
+        // Create a thread to run the Consumer
+        final Thread consumerThread = new Thread(consumer);
+        try {
+            consumerThread.start();
+
+            // Wait for startup
+            assertTimeout(ofSeconds(10), () -> {
+                while (!funnel.isRunning()) {
+                    Thread.sleep(100L);
+                }
+            }, "Timed out waiting for consumer thread to start.");
+
+            // Wait for messages to show up in the funnel
+            final List<Message> receivedMessages = new ArrayList<>();
+            assertTimeout(ofSeconds(10), () -> {
+                while (receivedMessages.size() < 30) {
+                    final Message nextMessage = funnel.nextMessage();
+                    if (nextMessage != null) {
+                        receivedMessages.add(nextMessage);
+                    }
+                }
+            }, "Timed out waiting to receive messages");
+
+            // Verify we got all the expected messages
+            assertEquals(30, receivedMessages.size(), "Should have gotten 30 messages.");
+
+            // Verify them
+            for (int index = 0; index < receivedMessages.size(); index++) {
+                final String foundId = receivedMessages.get(index).getId();
+                final String expectedId = "Id" + index;
+                assertEquals(expectedId, foundId);
+            }
+
+            // Call next message on funnel, it should have nothing
+            for (int counter = 0; counter < 10; counter++) {
+                assertNull(funnel.nextMessage(), "No more messages should be available");
+                Thread.sleep(100L);
+            }
+
+            // Now lets ack each message
+            receivedMessages.forEach((msg) -> funnel.ackMessage(msg.getId()));
+
+            // Wait a bit
+            Thread.sleep(3000L);
+
+            // Then request shutdown
+            funnel.requestStop();
+
+            // Wait for thread to stop.
+            assertTimeout(ofSeconds(10), (Executable) consumerThread::join, "Thread never stopped!");
+
+            // Verify interactions
+            verify(mockClient, times(1))
+                .connect();
+
+            // Received call to nextMessage at least 4 times
+            verify(mockClient, atLeast(4)).nextMessages();
+
+            // Received ack for each msg once
+            receivedMessages.forEach((msg) -> {
+                verify(mockClient, times(1)).commitMessage(eq(msg.getId()));
+            });
+
+            // Verify shutdown call
+            verify(mockClient, times(1)).disconnect();
+
+            // Verify funnel updated with status
+            assertFalse(funnel.isRunning());
+        } finally {
+            if (consumerThread.isAlive()) {
+                funnel.requestStop();
+                consumerThread.interrupt();
+                consumerThread.join(3000L);
+            }
+        }
+    }
+
+    private List<Message> createMessageBatch(final int count, int startingValue) {
+        final List<Message> messages = new ArrayList<>();
+        for (int index = 0; index < count; index++) {
+            final String messageId = "Id" + startingValue;
+            final Map<String, String> values = new HashMap<>();
+            values.put("Key", "Value" + startingValue);
+
+            messages.add(new Message(messageId, values));
+
+            startingValue++;
+        }
+
+        return messages;
+    }
+}
