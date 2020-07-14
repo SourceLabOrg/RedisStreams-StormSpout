458e12d8904015e9e99e04c023eb298971e3c268
Crim <stephen.powis@gmail.com>
2020-07-15 08:22:06 +0900

    Add funnel metrics

--- src/test/java/org/sourcelab/storm/spout/redis/RedisStreamSpoutIntegrationTest.java
+++ src/test/java/org/sourcelab/storm/spout/redis/RedisStreamSpoutIntegrationTest.java
@@ -32,6 +32,8 @@ import java.util.concurrent.TimeUnit;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
@@ -95,6 +97,10 @@ class RedisStreamSpoutIntegrationTest {
 
     @AfterEach
     void cleanup() {
+        // Verify standard metric interactions
+        verify(mockTopologyContext, times(3))
+            .registerGauge(anyString(), any());
+
         // Verify all mock interactions accounted for
         verifyNoMoreInteractions(mockTopologyContext);
     }
